19efdc73c63113ce049ed7e9c5e6f6f1ce9b96e3
ArumilliLalitha <arumillilalitha2005@gmail.com>
2025-07-04 20:19:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#typed", {
+      strings: ["a Python Developer", "a Web Developer", "a B.Tech Student"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
