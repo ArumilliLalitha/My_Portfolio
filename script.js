6595e2f8d9e78d9333573387adfd5db6b3ecd67c
ArumilliLalitha <arumillilalitha2005@gmail.com>
2025-07-05 19:01:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ var typed = new Typed("#typed", {
       backSpeed: 40,
       loop: true
     });
+const toggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+const navItems = document.querySelectorAll('#nav-links a');
+   toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+  navItems.forEach(link => {
+    link.addEventListener('click', () => {
+      navLinks.classList.remove('active');
+    });
+  });
